9d0a26fc91f99a2d72817e2cf51b01f36b327110
Andrew1399 <votintsev_99@mail.ru>
2022-03-27 10:56:54 +0300

    Task 1. Create lambda func

--- task1.py
+++ task1.py
@@ -1,1 +1,11 @@
-print('Hello Git!')
+from typing import Sequence
+import random
+import time
+from functools import reduce
+
+def my_func():
+    """Function lambda.
+    Take 2 numbers and raises a number to a power.
+    """
+    func = lambda a, x: a**x
+    print(func(2, 7))
