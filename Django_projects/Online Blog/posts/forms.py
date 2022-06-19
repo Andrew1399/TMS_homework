d0b10a3e77a86a880421a0473124de0981b83b3e
Andrew1399 <votintsev_99@mail.ru>
2022-06-19 18:05:45 +0300

    delete

--- Django_projects/Online Blog/posts/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from posts.models import Comment
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['name', 'email', 'content']
